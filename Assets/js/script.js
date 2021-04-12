76551cfca2d0a9fc9c85950ba116521d471072d3
Jeffrey Follestad <jeffrey.follestad@gmail.com>
2021-04-11 20:31:07 -0700

    I am stuck. Reverted changes I made earlier. Will need to resuvbmit this week

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -13,4 +13,43 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//adding to update
+window.alert("welcome to Password Generator!")
+/** Generates a password that is filled in the textarea. */
+function generatePassword() {
+  var result = "";
+  // 8 is the length of the password, hardcoded for now.
+  for (var i = 0; i < 8; i++) {
+    result += getRandomLetter();
+
+  }
+  return result;
+}
+
+/** Uses getRandomInt(max) below to get a random character from the alphabet. */
+function getRandomLetter() {
+  let alphabet = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ!#$%&'()*+,-./:;<=>?@[]^_`{|}~".split('')
+  let randomIndex = getRandomInt(alphabet.length - 1)
+  let letterResult = alphabet[randomIndex]
+  console.log('Letter at index ' + randomIndex + ' is ' + letterResult);
+  return letterResult
+}
+
+/** Gets a random integer between 0 and max. */
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+//random characters
+
+//function getRandomLower() {
+  //return String.fromCharCode(math.floor(math.random() * 26) + 97);
+
+//function getRandomUpper() {
+  //return String.fromCharCode(math.floor(math.random() * 26) + 65);
+
+//function getRandomNumber() {
+  //return String.fromCharCode(math.floor(math.random() * 10) + 48);
+
+//function getRandomSymbol() {
+    //const symbols = "!#$%&'()*+,-./|:;<=>?@[]^_`{|}"; 
+    //return symbols[math.floor(math.random() + symbols.length)
