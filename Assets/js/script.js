d96fe4ef2e5ffa89d133791b01a4188d416a3e4f
Jeff Follestad <jeffrey.follestad@gmail.com>
2021-08-18 22:02:47 -0500

    updating

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,13 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// assigned variables
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var special = "!@,#$%&*{}[]/\\+=";
+var chosenCharacters = "";
+
 // Write password to the #password input
-function writePassword() {
+function enterpassword() {
+  var password = genratepassword;
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+debugger;
+
+//write password function 
+function writePassword(){
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
